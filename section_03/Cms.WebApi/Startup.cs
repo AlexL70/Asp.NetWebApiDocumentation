adb99cfeb5c6fa9074f5d7ce57a9487a92f2a737
Alexander Levinson <alexander.levinson.70@gmail.com>
2023-08-28 13:02:22 +0300

    51. Using Generic Version Handler

--- section_03/Cms.WebApi/Startup.cs
+++ section_03/Cms.WebApi/Startup.cs
@@ -4,7 +4,7 @@ using Cms.WebApi.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,59 +30,50 @@ namespace Cms.WebApi
         {
             services.AddSingleton<ICmsRepository, InMemoryCmsRepository>();
             services.AddAutoMapper(typeof(CmsMapper));
-            _ = services.AddSwaggerGen(s =>
-            {
-                s.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "CMS Open API",
-                    Version = "v1",
-                    Description = "Open API Specificaton for CMS system",
-                    License = new OpenApiLicense
-                    {
-                        Name = "MIT",
-                    },
-                    Contact = new OpenApiContact
-                    {
-                        Email = "alexander.levinson.70@gmail.com",
-                        Name = "Alex Levinson"
-                    },
-                });
-                s.SwaggerDoc("v2", new OpenApiInfo
-                {
-                    Title = "CMS Open API",
-                    Version = "v2",
-                    Description = "Open API Specificaton for CMS system",
-                    License = new OpenApiLicense
-                    {
-                        Name = "MIT",
-                    },
-                    Contact = new OpenApiContact
-                    {
-                        Email = "alexander.levinson.70@gmail.com",
-                        Name = "Alex Levinson"
-                    },
-                });
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, $"{nameof(Cms)}.{nameof(WebApi)}.xml");
-                s.IncludeXmlComments(xmlPath);
-            });
-
             services.AddControllers(c =>
             {
                 //    c.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
             });
+
             services.AddApiVersioning(opt =>
-            {
-                opt.AssumeDefaultVersionWhenUnspecified = true;
-                opt.DefaultApiVersion = new ApiVersion(1, 0);
-            });
+             {
+                 opt.AssumeDefaultVersionWhenUnspecified = true;
+                 opt.DefaultApiVersion = new ApiVersion(1, 0);
+             });
             services.AddVersionedApiExplorer(opt =>
             {
+                opt.GroupNameFormat = "'v'V";
                 opt.SubstituteApiVersionInUrl = true;
+            });
+
+            var apiVersionDescr = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+            _ = services.AddSwaggerGen(s =>
+            {
+                foreach (var version in apiVersionDescr.ApiVersionDescriptions)
+                {
+                    s.SwaggerDoc(version.GroupName, new OpenApiInfo
+                    {
+                        Title = "CMS Open API",
+                        Version = version.GroupName,
+                        Description = "Open API Specificaton for CMS system",
+                        License = new OpenApiLicense
+                        {
+                            Name = "MIT",
+                        },
+                        Contact = new OpenApiContact
+                        {
+                            Email = "alexander.levinson.70@gmail.com",
+                            Name = "Alex Levinson"
+                        },
+                    });
+                }
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, $"{nameof(Cms)}.{nameof(WebApi)}.xml");
+                s.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider apiVersionDescr)
         {
             if (env.IsDevelopment())
             {
@@ -94,8 +85,10 @@ namespace Cms.WebApi
             app.UseSwagger();
             app.UseSwaggerUI(act =>
             {
-                act.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                act.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+                foreach (var version in apiVersionDescr.ApiVersionDescriptions)
+                {
+                    act.SwaggerEndpoint($"/swagger/{version.GroupName}/swagger.json", version.GroupName);
+                }
             });
 
             app.UseRouting();
